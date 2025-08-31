0d6216b0f50ab825158fdb1187de829dd8e92eb8
Smrutishree Naik <smrutishree.naik123@gmail.com>
2025-08-31 18:21:22 +0530

    added react-bootstrap

--- portfolio-app/src/App.tsx
+++ portfolio-app/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
+import { Button } from 'react-bootstrap';
 
 function App() {
 
   return (
     <>
       Hello there
+      <Button variant='primary'>click me</Button>
     </>
   )
 }
