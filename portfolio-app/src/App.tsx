62b48238e408f3b9f0268fecfc591a60fdc7938e
Smrutishree Naik <smrutishree.naik123@gmail.com>
2025-09-17 21:06:26 +0530

    updated nav links

--- portfolio-app/src/App.tsx
+++ portfolio-app/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import AboutAndExperience from './pages/AboutAndExperience';
@@ -40,27 +40,31 @@ const App: React.FC = () => {
     element.classList.add("shake");
   };
 
+    const [activeKey, setActiveKey] = useState<string>("home");
 
   return (
     <>
       <Navbar className="transparent-navbar" expand="lg" sticky="top">
-        <Container>
-          <Navbar.Brand onClick={() => handleScroll(homeRef)}>My Portfolio</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link onClick={() => handleScroll(homeRef)}>Home</Nav.Link>
-              <Nav.Link onClick={() => handleScroll(aboutRef)}>About</Nav.Link>
-              <Nav.Link onClick={() => handleScroll(experienceRef)}>Experience</Nav.Link>
-              <Nav.Link onClick={() => handleScroll(projectsRef)}>Projects</Nav.Link>
-              <Nav.Link onClick={() => handleScroll(caseStudyRef)}>Case Study</Nav.Link>
-              <Nav.Link onClick={() => handleScroll(testimonialRef)}>Testimonials</Nav.Link>
-              <Nav.Link onClick={() => handleScroll(contactRef)}>Contact</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-
+      <Container>
+        <Navbar.Brand>My Portfolio</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav
+            className="ms-auto"
+            activeKey={activeKey}
+            onSelect={(selectedKey) => setActiveKey(selectedKey || "home")}
+          >
+            <Nav.Link eventKey="home" onClick={() => handleScroll(homeRef)}>Home</Nav.Link>
+            <Nav.Link eventKey="about" onClick={() => handleScroll(aboutRef)}>About</Nav.Link>
+            <Nav.Link eventKey="experience" onClick={() => handleScroll(experienceRef)}>Experience</Nav.Link>
+            <Nav.Link eventKey="projects" onClick={() => handleScroll(projectsRef)}>Projects</Nav.Link>
+            <Nav.Link eventKey="caseStudy" onClick={() => handleScroll(caseStudyRef)}>Case Study</Nav.Link>
+            <Nav.Link eventKey="testimonials" onClick={() => handleScroll(testimonialRef)}>Testimonials</Nav.Link>
+            <Nav.Link eventKey="contact" onClick={() => handleScroll(contactRef)}>Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
       <div ref={homeRef} className="vh-100 d-flex align-items-center justify-content-center">
         <Home />
       </div>
