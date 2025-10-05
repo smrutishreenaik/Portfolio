d302b38c5c0379d01fdca14a96f53792e77574a4
Smrutishree Naik <smrutishree.naik123@gmail.com>
2025-10-05 16:10:56 +0530

    fixed css

--- portfolio-app/src/App.tsx
+++ portfolio-app/src/App.tsx
@@ -12,7 +12,7 @@ const App: React.FC = () => {
   return (
     <>
       <CustomNavbar />
-      <div id="home" className="vh-100 d-flex align-items-center justify-content-center">
+      <div id="home" className="vh-100 d-flex align-items-center justify-content-center home-ctn">
         <Home />
       </div>
 
