1f07e6995340b545fb7e01bb60be7bcc5a0d846d
Smrutishree Naik <smrutishree.naik123@gmail.com>
2025-10-07 06:18:41 +0530

    fixed css

--- portfolio-app/src/components/Experience.tsx
+++ portfolio-app/src/components/Experience.tsx
@@ -37,27 +37,29 @@ const Experience: React.FC = () => {
     <Container>
       <LoopingVideo videoSrc={ExpVideo} videoClassName='exp-video' />
       <div className="d-flex text-start mb-4">
-        <h1 className='fw-bold display-5'>
+        <h1 className='fw-bold display-5 mx-auto'>
           <span className="text-muted">My Past</span> <br />
           Work Experience
         </h1>
       </div>
 
-      <div>
+      <div className='d-flex flex-column'>
         {timeline.map((item, index) => (
-          <div key={index} className="d-flex align-items-center mb-4">
-            <img
-              src={item.logo}
-              alt={item.company}
-              width={40}
-              height={40}
-              className="me-3 circular-img"
-            />
-            <div>
-              <h5 className="mb-1">{item.role}</h5>
-              <p className="mb-0 text-muted">
-                {item.company}, {item.duration}
-              </p>
+          <div key={index} className='d-flex mx-auto mb-4'>
+            <div className="d-flex">
+              <img
+                src={item.logo}
+                alt={item.company}
+                width={40}
+                height={40}
+                className="me-3 circular-img"
+              />
+              <div className='text-start exp-ctn'>
+                <h5 className="mb-1">{item.role}</h5>
+                <p className="mb-0 text-muted">
+                  {item.company}, {item.duration}
+                </p>
+              </div>
             </div>
           </div>
         ))}
